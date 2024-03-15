0f55ad3358f680530b47a1935bb9bfd5fd2711d3
Neil Wu <Neil@fakemail.com>
2024-03-15 16:12:42 +0800

    更新測試

---
+++ src/components/Event/Main/Btn/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="btn-wrap">
+    <div
+      class="btn-start"
+      :style="{
+        'background-image': `url(${require('@/assets/alert_btn_highlight.png')})`,
+      }"
+    ></div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
--- src/components/Event/Main/index.vue
+++ src/components/Event/Main/index.vue
@@ -2,12 +2,14 @@
   <div class="main-wrap">
     <Background />
     <Rotate />
+    <Btn />
     <!-- <TestSix /> -->
   </div>
 </template>
 <script lang="ts" setup>
 import Background from "../Main/Background/index.vue";
 import Rotate from "../Main/Rotate/index.vue";
+import Btn from "../Main/Btn/index.vue";
 // import TestSix from "../Main/TestSix/index.vue";
 </script>
 <style lang="scss" scoped>
