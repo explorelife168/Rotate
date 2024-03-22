ef18c1db93c97bc73ae7b03d5500261ab9587b18
Neil Wu <Neil@fakemail.com>
2024-03-22 23:17:48 +0800

    測試成功

--- src/components/Event/Main/Map/index.vue
+++ src/components/Event/Main/Map/index.vue
@@ -25,18 +25,11 @@ const getShowAwardStatus = computed(() => dataStore.getShowAwardStatus);
 
 const initMap = () => {
   const google: undefined | any = (window as any).google;
-  const myLatLng = { lat: 24.112316131591797, lng: 120.61644744873047 };
-  let maps: any = new google.maps.Map(map.value, {
+  const myLatLng = { lat: 24.92218475939543, lng: 121.2141211272122 };
+  new google.maps.Map(map.value, {
     center: myLatLng,
     zoom: 14,
   });
-
-  let marker = new google.maps.marker.AdvancedMarkerElement({
-    position: myLatLng,
-    map: maps,
-    title: "hello",
-  });
-  marker.setPosition();
 };
 
 watch(getShowAwardStatus, (newVal) => {
